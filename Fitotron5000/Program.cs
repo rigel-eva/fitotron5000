1c9fb85d561bee827c5ff942ea5545d336db5ffb
Alex Maguire <eva@rigel.moe>
2018-03-03 17:59:59 -0600

    Add project files.

---
+++ Fitotron5000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fitotron5000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
