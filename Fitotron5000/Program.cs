f12579b6aabdda3a313be8ddae95d8f687d9b2ae
Alex Maguire <eva@rigel.moe>
2018-03-03 21:16:55 -0600

    Ok, so Fitotron is a basic Discord ping bot at the moment
    I think it's high time we change that~~

--- Fitotron5000/Program.cs
+++ Fitotron5000/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
-
+﻿using Discord;
+using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
 namespace Fitotron5000
 {
-    class Program
+
+    internal class Program
     {
-        static void Main(string[] args)
+
+        private const string commandPrefix = "!";
+        private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+            //Note we are expecting the enviroment variable in the user profile.
+            string token = System.Environment.GetEnvironmentVariable("fitotronkey",EnvironmentVariableTarget.User); // Remember to keep this private!
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content.ToUpper() == "!HARD")
+            {
+                await message.Channel.SendMessageAsync("AS NAILS!");
+            }
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
