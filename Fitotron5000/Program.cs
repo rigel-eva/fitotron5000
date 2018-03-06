ce21984b5c3247618610c676edb4e345ce7e58f1
Nora Maguire <eva@rigel.moe>
2018-03-06 17:55:14 -0600

    Ok, Fixed %copyconstructor
    As in Fixed fixed ... as in it works now without creating an infinite loop.
    
    Also stopped myself from creating the same thing that I have here but with extra steps
    
    *well barbadookel looks like someone is gonna get laid in university*

--- Fitotron5000/Program.cs
+++ Fitotron5000/Program.cs
@@ -1,21 +1,20 @@
 ﻿using Discord;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Discord.WebSocket;
 namespace Fitotron5000
 {
 
     internal class Program
     {
-
         private const string commandPrefix = "%";
         private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         public async Task MainAsync()
         {
             var client = new DiscordSocketClient();
             client.Log += Log;
-            client.MessageReceived += MessageReceived;
-            client.MessageReceived += Ping;
+            client.MessageReceived += messageHandler;
             //client.MessageReceived += CopyConstructor;
             //Note we are expecting the enviroment variable in the user profile.
             string token = System.Environment.GetEnvironmentVariable("fitotronkey"); // Remember to keep this private!
@@ -25,39 +24,43 @@ namespace Fitotron5000
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
-        private async Task MessageReceived(SocketMessage message)
-        {
-            if (isCommand(message,"hard"))
-            {
-                await message.Channel.SendMessageAsync("AS NAILS!");
+         private bool isCommand(SocketMessage message, string commandInvoker){
+            bool returner=!message.Author.IsBot; //Checking if our bot is not talking to another bot, because that would be dumb.
+            Console.WriteLine("Checking if not a bot ... : "+returner.ToString());
+            Console.WriteLine("Possible Message: "+message.Content.ToLower());
+            Console.WriteLine("Message Check: "+message.Content.ToLower().Substring(0, (commandPrefix + commandInvoker).Length));
+            Console.WriteLine("Command looking for: "+commandPrefix + commandInvoker);
+            return (!message.Author.IsBot && message.Content.ToLower().Substring(0, (commandPrefix + commandInvoker).Length) == commandPrefix + commandInvoker);
+        }
+        private async Task messageHandler(SocketMessage message){
+            if(!message.Author.IsBot&&message.Content.Substring(0,commandPrefix.Length)==commandPrefix){
+                switch(message.Content.Split(" ")[0].ToLower()){
+                    case commandPrefix+"hard":
+                        await message.Channel.SendMessageAsync("AS NAILS!");
+                        break;
+                    case commandPrefix+"ping":
+                        await message.Channel.SendMessageAsync("Pong!");
+                        break;
+                    case commandPrefix+"pong":
+                        await message.Channel.SendMessageAsync("Ping!");
+                        break;
+                    case commandPrefix+"copyconstructor":
+                        CopyConstructor(message);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        private async Task Ping(SocketMessage message)
+        private void CopyConstructor(SocketMessage message)
         {
-            if (isCommand(message,"ping"))
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
-            else if (isCommand(message, "pong"))
-            {
-                await message.Channel.SendMessageAsync("Ping!");
-            }
-        }
-        private async Task CopyConstructor(SocketMessage message)
-        {
-           if (isCommand(message,"copyconstructor"))
-           {
-               await message.Channel.SendMessageAsync(message.Content);
-           }
+               message.Channel.SendMessageAsync(message.Content);
         }
         private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
-        private bool isCommand(SocketMessage message, string commandInvoker){
-            bool returner=!message.Author.IsBot; //Checking if our bot is not talking to another bot, because that would be dumb.
-            return (!message.Author.IsBot && message.Content.ToLower().Substring(0, (commandPrefix + commandInvoker).Length) == commandPrefix + commandInvoker);
-        }
+       
     }
 }
