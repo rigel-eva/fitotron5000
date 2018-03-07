ad7ae6ef25a1691e0e3541d9bf2c6affb0374157
Nora Maguire <eva@rigel.moe>
2018-03-06 19:30:29 -0600

    Ok, added Model Code
    ... I think ... (why do I have a feeling I'm gonna be spending a significant ammount of time debugging this ...)

---
+++ Fitotron5000/User.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+namespace Fitotron5000{
+    public class UserContext :DbContext{
+        public DbSet<User> Users {get; set;}
+        public DbSet<Weight> Weights {get;set;}
+    }
+    public class User{
+        public int discordID;
+        public decimal goal;
+        public List<Weight> weights;
+    }
+    public class Weight{
+        public int discordID;
+        public int weightID;
+        public User user;
+        public decimal userWeight;
+        public DateTime timeStamp;
+    }
+}
