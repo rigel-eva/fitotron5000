36b069850fb30d178b9ed7036c7b6e01862d39db
Nora Maguire <eva@rigel.moe>
2018-03-07 00:25:26 -0600

    Ok, so far so good
    Need to probably read up on this module a bit more ... nyo well~ 
    
    I could stand to sleep right meow ... goodnight!

--- Fitotron5000/User.cs
+++ Fitotron5000/User.cs
@@ -5,23 +5,30 @@ namespace Fitotron5000{
     public class UserContext :DbContext{
         public DbSet<User> Users {get; set;}
         public DbSet<Weight> Weights {get;set;}
-        
          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
              optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=fitotron_dev;Trusted_Connection=True;");
+         }
+         protected override void OnModelCreating(ModelBuilder modelBuilder){
+             modelBuilder.Entity<User>(entity=>{
+                 entity.Property(e=>e.discordID).IsRequired();
+             });
+             modelBuilder.Entity<Weight>(entity=>{
+                 entity.HasOne(u=>u.user).WithMany(w=>w.weights).HasForeignKey(u=>u.Id);
+             });
          }
     }
     public class User{
         public int Id{get; set;}
-        public ulong discordID;
-        public decimal goal;
-        public decimal currentWeight;
-        public List<Weight> weights;    
+        public ulong discordID{get; set;}
+        public decimal goal{get; set;}
+        public decimal currentWeight{get; set;}
+        public List<Weight> weights{get; set;}
     }
     public class Weight{
-        public ulong discordID;
+        public int userID{get; set;}
         public int Id{get;set;}
-        public User user;
-        public decimal userWeight;
-        public DateTime timeStamp;
+        public User user{get; set;}
+        public decimal userWeight{get; set;}
+        public DateTime timeStamp{get; set;}
     }
 }
