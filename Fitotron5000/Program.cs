3c91d6d6b7964c3aa7d62e9d657c5c3f8471e42e
Nora Maguire <eva@rigel.moe>
2018-03-06 18:42:02 -0600

    Woot! Got command line arguments working

--- Fitotron5000/Program.cs
+++ Fitotron5000/Program.cs
@@ -9,28 +9,31 @@ namespace Fitotron5000
     internal class Program
     {
         private const string commandPrefix = "%";
-        private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-        public async Task MainAsync()
+        private static void Main(string[] args) => new Program().MainAsync(args).GetAwaiter().GetResult();
+        public async Task MainAsync(string[] args)
         {
+            string token="";//Where our token will be eventually stored
+            for(int i =0; i<args.Length; i++){
+                switch(args[i].Split(" ")[0]){
+                    case "--discordKey":
+                        Console.WriteLine($"{DateTime.Now.ToString("h:mm:ss")}Info\tUsing Key from Command Line");
+                        token=args[i].Split(" ")[1];//ok, we want to grab the next argument in the set and skip past everything else
+                        break;
+                }
+            }
             var client = new DiscordSocketClient();
             client.Log += Log;
             client.MessageReceived += messageHandler;
             //client.MessageReceived += CopyConstructor;
             //Note we are expecting the enviroment variable in the user profile.
-            string token = System.Environment.GetEnvironmentVariable("fitotronkey"); // Remember to keep this private!
+            if(token==""){
+                token = System.Environment.GetEnvironmentVariable("fitotronkey");
+            }
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
-        }
-         private bool isCommand(SocketMessage message, string commandInvoker){
-            bool returner=!message.Author.IsBot; //Checking if our bot is not talking to another bot, because that would be dumb.
-            Console.WriteLine("Checking if not a bot ... : "+returner.ToString());
-            Console.WriteLine("Possible Message: "+message.Content.ToLower());
-            Console.WriteLine("Message Check: "+message.Content.ToLower().Substring(0, (commandPrefix + commandInvoker).Length));
-            Console.WriteLine("Command looking for: "+commandPrefix + commandInvoker);
-            return (!message.Author.IsBot && message.Content.ToLower().Substring(0, (commandPrefix + commandInvoker).Length) == commandPrefix + commandInvoker);
         }
         private async Task messageHandler(SocketMessage message){
             if(!message.Author.IsBot&&message.Content.Substring(0,commandPrefix.Length)==commandPrefix){
