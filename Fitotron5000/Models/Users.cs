4defc4e7d80f68a8bb0a459c434785b5e34c02dd
Nora Maguire <eva@rigel.moe>
2018-03-07 20:04:19 -0600

    Welp ... Thank you autogenerated code
    ... at least this is kinda working?

---
+++ Fitotron5000/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fitotron5000.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public long DiscordId { get; set; }
+        public decimal? Goal { get; set; }
+        public decimal? CurrentWeight { get; set; }
+    }
+}
---
+++ Fitotron5000/Models/Weights.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fitotron5000.Models
+{
+    public partial class Weights
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public decimal? UserWeight { get; set; }
+        public DateTime? TimeStamp { get; set; }
+    }
+}
